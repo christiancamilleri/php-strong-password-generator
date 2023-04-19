1902b1dee8cee13a112ee8fd1ede7d43d0a6b2d4
christiancamilleri <camilleri.christian@outlook.com>
2023-04-19 15:24:25 +0200

    feat:milestone 2

--- index.php
+++ index.php
@@ -1,44 +1,8 @@
 <?php
-// salvo in una variabile la lunghezza della password richiesta dall utente
-$passwordLength = $_GET['characterNumber'];
 
-// creo funzione che mi genera una password casuale
-function getRandomPassword($num)
-{
-    // creo un elenco di caratteri disponibili
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789?!$%&/@*#';
+require './partials/function.php'
 
-
-    // definiamo la lunghezza dei caratteri disponibili ($characters)
-    $characterslength = strlen($characters);
-    // var_dump($characterslength);
-
-    // dichiaro array password
-    $password = array();
-
-    // cicliamo x volte dove x è il numero scelto dall utente
-    for ($i = 0; $i < $num; $i++) {
-
-        // genero numero random che va da 0 al numero massimo di caratteri  
-        $randomNumber = rand(0, $characterslength - 1);
-        // var_dump($randomNumber);
-
-        // inserisco caratteri nell array 
-        $password[] = $characters[$randomNumber];
-
-    }
-
-    // trasformo l array password in una stringa
-    return implode($password);
-
-}
-
-// password generata 
-$randomPassword = getRandomPassword($passwordLength);
-
-
-
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -60,7 +24,15 @@ $randomPassword = getRandomPassword($passwordLength);
     </form>
 
     <?php
-    echo 'Ecco qui la tua password:' . '<h3>' . $randomPassword . '<h3>';
+    echo 'Ecco qui la tua password:';
+
+    if (isset($randomPassword)) {
+        echo '<h3>' . $randomPassword . '<h3>';
+    } else {
+        echo '<br> inserisci un numero qui sopra per ricevere la password';
+    }
+    ;
+
     ?>
 </body>
 
---
+++ partials/function.php
@@ -0,0 +1,48 @@
+<?php
+if (isset($_GET['characterNumber'])) {
+    // salvo in una variabile la lunghezza della password richiesta dall utente
+    $passwordLength = $_GET['characterNumber'];
+}
+
+
+
+
+// creo funzione che mi genera una password casuale
+function getRandomPassword($num)
+{
+    // creo un elenco di caratteri disponibili
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789?!$%&/@*#';
+
+
+    // definiamo la lunghezza dei caratteri disponibili ($characters)
+    $characterslength = strlen($characters);
+    // var_dump($characterslength);
+
+    // dichiaro array password
+    $password = array();
+
+    // cicliamo x volte dove x è il numero scelto dall utente
+    for ($i = 0; $i < $num; $i++) {
+
+        // genero numero random che va da 0 al numero massimo di caratteri  
+        $randomNumber = rand(0, $characterslength - 1);
+        // var_dump($randomNumber);
+
+        // inserisco caratteri nell array 
+        $password[] = $characters[$randomNumber];
+
+    }
+
+    // trasformo l array password in una stringa
+    return implode($password);
+
+}
+
+if (isset($passwordLength)) {
+    // password generata 
+    $randomPassword = getRandomPassword($passwordLength);
+
+}
+
+
+?>
